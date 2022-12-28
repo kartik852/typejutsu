1a7bf0cb4a551e2f4d3cdefdffc9ead346d01849
unknown <softech.series@gmail.com>
2022-12-28 15:17:50 +0530

    first commit

---
+++ src/componenets/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p
+                target="_blank"
+                className="footer-link">
+              Made By Kartik 😉   </p>
+        </div>
+    );
+};
+
+export default Footer;
